5338b8b11d57ea1ad9e55f2f7867d97ff1ff4c09
YuyoungRhee <bbclara@naver.com>
2024-11-22 01:16:07 +0900

    feat(Application): 전체 프로그램 실행을 구현
    - 뷰와 컨트롤러를 생성하고 컨트롤러에 뷰를 주입, 실행하는 기능 구현

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,17 @@
 package oncall;
 
+import oncall.controller.InputHandler;
+import oncall.controller.WorkController;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        InputHandler inputHandler = new InputHandler(inputView);
+        OutputView outputView = new OutputView();
+
+        WorkController controller = new WorkController(inputHandler, outputView);
+        controller.run();
     }
 }
